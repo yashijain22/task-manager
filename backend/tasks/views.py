b971b7f335d48ccf108dce8006ff937b0dbd1be1
yashijain22 <yashi2212.jain@gmail.com>
2026-02-18 17:53:55 +0530

    Initial commit - Task Manager

---
+++ backend/tasks/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
